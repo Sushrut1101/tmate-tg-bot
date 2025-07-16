9bcba2181c86eb1db224427cf06eac4ba035f54b
Sushrut1101 <guptasushrut@gmail.com>
2025-07-17 02:14:33 +0530

    bot: Wrap in `main()`
    Change-Id: Iaf985dc1625508c36c052b230d3a1d660540c2d4
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- src/main.py
+++ src/main.py
@@ -3,18 +3,19 @@ from dotenv import load_dotenv
 from telegram.ext import ApplicationBuilder, CommandHandler
 from src.modules import commands
 
-load_dotenv(dotenv_path=os.path.join(os.getcwd(), ".env"))
-token = os.getenv("BOT_TOKEN")
+def main():
+    load_dotenv(dotenv_path=os.path.join(os.getcwd(), ".env"))
+    token = os.getenv("BOT_TOKEN")
 
-if not token:
-    raise ValueError("BOT_TOKEN environment variable not set")
+    if not token:
+        raise ValueError("BOT_TOKEN environment variable not set")
 
-app = ApplicationBuilder().token(token).build()
-
-app.add_handler(CommandHandler("new", commands.handle_new))
-app.add_handler(CommandHandler("list", commands.handle_list))
-app.add_handler(CommandHandler("kill", commands.handle_kill))
-app.add_handler(CommandHandler("show", commands.handle_show))
+    app = ApplicationBuilder().token(token).build()
+    app.add_handler(CommandHandler("new", commands.handle_new))
+    app.add_handler(CommandHandler("list", commands.handle_list))
+    app.add_handler(CommandHandler("kill", commands.handle_kill))
+    app.add_handler(CommandHandler("show", commands.handle_show))
+    app.run_polling()
 
 if __name__ == "__main__":
-    app.run_polling()
+    main()
