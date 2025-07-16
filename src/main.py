65756830188c6220691f660074387af7221bcf0c
Sushrut1101 <guptasushrut@gmail.com>
2025-07-17 02:03:17 +0530

    bot: Use `dotenv`
    Change-Id: I9ddca82e9e19c60b1f6cdbacfffde9bbd844fb6f
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
+import os
+from dotenv import load_dotenv
 from telegram.ext import ApplicationBuilder, CommandHandler
 from src.modules import commands
 
-app = ApplicationBuilder().token("").build()
+load_dotenv(dotenv_path=os.path.join(os.getcwd(), ".env"))
+token = os.getenv("BOT_TOKEN")
+
+if not token:
+    raise ValueError("BOT_TOKEN environment variable not set")
+
+app = ApplicationBuilder().token(token).build()
 
 app.add_handler(CommandHandler("new", commands.handle_new))
 app.add_handler(CommandHandler("list", commands.handle_list))
